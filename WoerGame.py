b3c9a31890645b54a043066ccaabf27a81f50153
Sorin Hadarau <sorin.hadarau@gmail.com>
2023-01-11 01:28:17 +0000

    Create WoerGame.py

---
+++ WoerGame.py
@@ -0,0 +1,26 @@
+import collections
+from PyDictionary import PyDictionary
+
+sourceword = "exampleword"
+sourceword = sourceword.lower() # convert sourceword to lowercase
+
+# Create a frequency dictionary for the sourceword
+freq_dict = collections.Counter(sourceword)
+
+# Create an empty list to store the words
+seven_words = []
+dictionary=PyDictionary()
+
+# Iterate through a dictionary of words
+with open("words_dictionary.txt") as f:
+    for word in f:
+        word = word.strip()
+        word = word.lower() # make word case-insensitive
+        if (len(word) >= 4) and (word != sourceword) and (collections.Counter(word) <= freq_dict):
+            if dictionary.meaning(word):
+                seven_words.append(word)
+
+# Remove duplicate words from the list
+seven_words = list(set(seven_words))
+
+print(seven_words)
